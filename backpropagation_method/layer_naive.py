b73c9c6cc0dcf5f7796eecd48b67eaa2ac80e927
yusnmt <yusnmt@gmail.com>
2021-07-24 23:38:42 +0900

    create class Relu and create class Sigmoid

--- backpropagation_method/layer_naive.py
+++ backpropagation_method/layer_naive.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MulLayer:
     def __init__(self):
         self.x = None
@@ -26,3 +28,34 @@ class AddLayer:
         dx = dout * 1
         dy = dout * 1
         return dx, dy
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
