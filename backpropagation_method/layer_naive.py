cfcdf7e4558a7cb24daf063b63d464f3fdc1b9e3
yusnmt <yusnmt@gmail.com>
2021-07-27 23:34:35 +0900

    create class SoftmaxWithLoss

--- backpropagation_method/layer_naive.py
+++ backpropagation_method/layer_naive.py
@@ -80,3 +80,22 @@ class Affine:
         self.db = np.sum(dout, axis=0)
 
         return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None #損失
+        self.y = None    #softmaxの出力
+        self.t = None    # 教師データ（one-hot vector)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
